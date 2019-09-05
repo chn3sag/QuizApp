cba7ed017fdc5a2f7fb0286857525746f01d5e0e
Chandrika <chandrisag@gmail.com>
2019-09-05 15:12:49 -0400

    Initial files. Added index.html, main.js, datastore.js

---
+++ datastore.js
@@ -0,0 +1,18 @@
+const DATA = [
+    {
+        question:" ",
+        options: [],
+        answer:" "
+    },
+    {
+        question: " ",
+        options: [],
+        answer: " " 
+    },
+    {
+
+    }
+]
+
+let correctAns = 0;
+let numQuestion = 1;
