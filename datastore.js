1f7e1a2bc8b3893d611d2b6ec44b97691a13dd23
Chandrika <chandrisag@gmail.com>
2019-09-16 15:33:31 -0400

    Working Quiz App

--- datastore.js
+++ datastore.js
@@ -1,55 +1,55 @@
 const DATA = [
     {
-        question: "What is the function of Array object that adds and/or removes elements from an array?",
+        question: "What is the function of Array object that adds and/or removes elements from an array ?",
         options: ["slice", "replace", "splice", "split"],
         answer: "splice"
     },
     {
-        question: "Which of the following methods removes the last element from an array and returns that element? ",
+        question: "Which of the following methods removes the last element from an array and returns that element ? ",
         options: ["pop", "last", "end", "shift"],
         answer: "pop"
     },
     {
-        question: "What is the output of \n\t []+\"bar\".split(\"\") ",
+        question: "What is the output of \n\t []+\"bar\".split(\"\") ?",
         options: ["[,'b','a','r']", "b,a,r", "undefined", NaN],
         answer: "b,a,r"
     },
     {
-        question: "What is the output of \n\t true+false+1 ",
-        options: ["undefined", 1, 2, "NaN"],
+        question: "What is the output of \n\t true+false+1 ?",
+        options: ["undefined", "1", "2", "NaN"],
         answer: "2"
     },
     {
-        question: "Which function is used to copy elements to a different index in an array",
+        question: "Which function is used to copy elements to a different index in an array ?",
         options: ["replace", "repeat", "copyWithin", "fill"],
         answer: "copyWithin"
     },
     {
-        question: "What is the output of \n\t 1===true ",
+        question: "What is the output of \n\t 1===true ?",
         options: ["true", "false"],
         answer: "false"
     },
     {
-        question: "How should you declare a variable which is not expected to change?",
+        question: "How should you declare a variable which is not expected to change ?",
         options: ["let", "var", "const", "no keyword"],
         answer: "const"
     },
     {
-        question: "How do you remove leading and trailing whitespaces from a string? ",
+        question: "How do you remove leading and trailing whitespaces from a string ? ",
         options: ["remove", "trim", "replace", "split"],
         answer: "trim"
     },
     {
-        question: "What function is used to check if any of the array values pass a test? ",
+        question: "What function is used to check if any of the array values pass a test ?",
         options: ["some", "any", "every", "all"],
         answer: "some"
     },
     {
-        question: " What is the output of NaN===NaN",
+        question: " What is the output of NaN===NaN ?",
         options: ["true", "false"],
         answer: "false"
     }
-]
+];
 
-let correctAns = 0;
-let numQuestion = 1;
+let score = 0;
+let numQuestion = 0;
---
+++ main.js
@@ -0,0 +1,205 @@
+/**
+ * This function is to set the quiz initially for users;
+ */function startQuiz() {
+     console.log("-->In startQuiz");
+     $(".questionNo-score").hide();
+     $(".js-start-quiz-btn").click(function(){
+        //This starts the quiz.
+        $(".intro-box").hide();
+        $(".js-qtn-answer").hide();
+        $(".js-restart-quiz").hide();
+        $(".question-option-form").show();
+        createQuestionOptionFormContent();
+     });
+}
+
+/** Creates content for the question and its options */
+function createQuestionOptionFormContent() {
+    console.log("-->In createQuestionOptionFormContent");
+    updateQuestionAndScore();
+    let question = DATA[numQuestion].question;
+    let myQuestionForm = $(`
+    <form class="quiz-question">
+        <fieldset>
+            <legend class="question"> ${question} </legend>
+            <section class="options">
+                <ul></ul>
+            </section>
+            <button type="submit" class="user-ctrl-btn">Submit</button>
+        </fieldset>
+    </form>`);
+    $(".question-option-form").html(myQuestionForm);
+    addOptions(myQuestionForm);
+}
+
+/** Add quiz answer options to the form */
+function addOptions(myQuestionForm) {
+    console.log("-->In addOptions");
+    for (let ans in DATA[numQuestion].options ) {
+        $(myQuestionForm).find(".options").find("ul").append( 
+        `<li> 
+        <input type="radio" name="answers" id="${ans}" value="${ans}">
+        <label for="${ans}"> ${DATA[numQuestion].options[ans]}</label>
+        </li >`);
+    }
+}
+
+/** Validates the user selected option
+ * @selectedOption: Index of user selected option
+ */
+function checkAnswer(selectedOption) {
+    console.log("-->In checkAnswer");
+    if (!selectedOption) {
+        alert("Please select an option to proceed");
+        return;
+    }
+    $(".question-option-form").hide();
+    if (DATA[numQuestion].answer === DATA[numQuestion].options[selectedOption]) {
+        score++;
+        updateScore();
+        updateCorrectAnswer();
+    } else {
+        updateIncorrectAnswer(); 
+    }
+}
+
+/** Updates the HTML with correct Answer Screen */
+function updateCorrectAnswer() {
+    let correctAns = $(`
+    <h2>Yay! That is correct!!!</h2>
+    <img class="right-answer-img img-border" src="images/CorrectAnswer.png" alt="Correct Answer Image">
+    `); 
+    $(".js-qtn-answer").html(correctAns);
+    updateAnsCheckContent();
+}
+
+/** Updates the HTML with incorrect Answer Screen */
+function updateIncorrectAnswer() {
+    let inCorrectAns = $(`
+    <h2>Uh-Oh! That is Incorrect!!!</h2>
+    <img class="wrong-answer-img img-border" src="images/IncorrectAns.jpg" alt="InCorrect Answer Image">
+    `);
+    $(".js-qtn-answer").html(inCorrectAns);
+    updateAnsCheckContent();
+}
+
+/** Updates the answer content */
+function updateAnsCheckContent() {
+    let btnContent = $(`
+        <h2 class="right-answer">The correct answer is ${DATA[numQuestion].answer}</h2>
+        <button class="user-ctrl-btn js-next-btn">Next</button>`);
+    $(".js-qtn-answer").append(btnContent);
+    $(".js-qtn-answer").show();
+}
+
+/** Update Content with new question */
+function moveToNextQuestion() {
+    createQuestionOptionFormContent();
+    $(".question-option-form").show();
+}
+
+/** Update content with final page */
+function moveToEndScreen() {
+    createRestartQuiz();
+}
+
+/** Create content for end of quiz and restart option */
+function createRestartQuiz() {
+    let performance = "";
+    if (score >= DATA.length -2) { 
+        performance = "Yay! You did great!!!";
+    } else if (score < (DATA.length/2)) {
+        performance = "Better luck next time!"
+    } else if (score >= (DATA.length / 2) && (score < DATA.length - 2)) { 
+        performance = "You did well but can do better!";
+    }
+    let endQuiz = $(`
+        <h2>Your score is: ${score}</h2>
+        <h2>${performance}</h2>
+        <section class="buttons">
+            <button class="user-ctrl-btn js-restart-btn">Restart Quiz</button>
+            <button class="user-ctrl-btn js-end-quiz-btn">End Quiz</button>
+        </section>
+    `); 
+    $(".js-restart-quiz").show();
+    $(".js-restart-quiz").html(endQuiz);
+}
+
+/**Reset the question num and score values and hide the content on screen */
+function resetAndHideQuestionAndScore() {
+    numQuestion = 0;
+    score = 0;
+    $(".questionNo-score").hide();
+}
+
+/** Update the question number and score on the screen */
+function updateQuestionAndScore() {
+    $(".questionNo-score").show();
+    updateQuestionNum();
+    updateScore();
+}
+
+/** Update the question Number */
+function updateQuestionNum() {
+    $(".qNum").text(`${numQuestion+1}`);
+}
+
+/** Update the score */
+function updateScore() {
+    $(".score").text(`${score}`);
+}
+
+/** Add event listener to submit button */
+function handleSubmitAns() {
+    $('.question-option-form').on("submit",".quiz-question",function (event) {
+        event.preventDefault();
+        let selectedOption = $('input[name=answers]:checked').val();
+        checkAnswer(selectedOption);
+    });
+}
+
+/** Add event listener on next button */
+function handleNextQtn() {
+    $(".js-qtn-answer").on("click", ".js-next-btn", function (event) {
+        event.preventDefault();
+        console.log("-->In callback of next btn");
+        $(".js-qtn-answer").hide();
+        if ((numQuestion + 1) === (DATA.length)) {
+            moveToEndScreen();
+        } else {
+            numQuestion++;
+            console.log("-->NumQuestion:" + numQuestion);
+            moveToNextQuestion();
+        }
+    });
+}
+
+/** Event listener on restart quiz button */
+function handleRestart() {
+    $('.js-restart-quiz').on("click", ".js-restart-btn", function (event) {
+        $(".js-restart-quiz").hide();
+        resetAndHideQuestionAndScore();
+        createQuestionOptionFormContent();
+        $(".question-option-form").show();
+    });
+}
+
+/** Event listener on end quiz button */
+function handleEndQuiz()  {
+    $('.js-restart-quiz').on("click", ".js-end-quiz-btn", function (event) {
+        $(".js-restart-quiz").hide();
+        resetAndHideQuestionAndScore();
+        $(".intro-box").show();
+    })
+}
+
+/** Run the quiz application*/
+function quizApp() {
+    startQuiz();
+    handleSubmitAns();
+    handleNextQtn();
+    handleRestart();
+    handleEndQuiz();
+}
+
+$(quizApp);
